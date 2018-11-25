0707e0a12d9be08c62c828b2d9b4dd9516fb148c
Jimmy Pan <pantc12@gmail.com>
2018-11-25 12:09:52 +0800

    Code refactoring

--- main_dirty.py
+++ main_dirty.py
@@ -9,16 +9,16 @@ last_count_diff = 0
 
 try:
     while(True):
-        os.system('clear')
-        html = requests.get(url).text
-        soup = BeautifulSoup(html, 'html.parser')
-
         data = []
+
+        html = requests.get(url).text
+
+        soup = BeautifulSoup(html, 'html.parser')
         div = soup.find('div', attrs={'class': 'table-content'})
         table = div.find('table')
         table_body = table.find('tbody')
-
         rows = table_body.find_all('tr')
+
         for row in rows:
             cols = row.find_all('td')
             cols = [ele.text for ele in cols]
@@ -27,21 +27,26 @@ try:
         ding = [data[1][2], data[1][4].replace(',', '')]
         kp  =  [data[3][2], data[3][4].replace(',', '')]
 
-        print(': '.join(ding))
-        print(': '.join(kp))
         diff = int(kp[1]) - int(ding[1])
+        sign = '+' if diff > 0 else '-'
+        rate = round(abs(int(kp[1]) - int(ding[1])) / (int(kp[1]) + int(ding[1])) * 100, 2)
+
         if(diff != last_count):
             last_count_diff = diff - last_count
             last_count = diff
-        sign = '+' if diff > 0 else '-'
-        diff_per = abs(int(kp[1]) - int(ding[1])) / (int(kp[1]) + int(ding[1])) * 100
-        print('柯文哲 VS 丁守中:', sign + str(diff) + '(' + str(last_count_diff) + ')', '(' + str(round(diff_per, 2)) + '%)')
 
         footer = soup.find('div', attrs={'class': 'table-footer-right'})
-        info_raw = footer.text.strip().replace(u'\xa0', ' ').split()[1:]
+        info_raw = footer.text.replace(u'\xa0', ' ').split()[1:]
         send = info_raw[1].split('/')
-        info = [' '.join(info_raw[0:2]) + ' (' + str(int(send[1]) - int(send[0])) + ')', ' '.join(info_raw[2:])]
+        send_diff = int(send[1]) - int(send[0])
+        info = [' '.join(info_raw[0:2]) + ' (' + str(send_diff) + ')', ' '.join(info_raw[2:])]
+
+        os.system('clear')
+        print(': '.join(ding))
+        print(': '.join(kp))
+        print('柯文哲 VS 丁守中:', sign + str(diff) + '(' + str(last_count_diff) + ')', '(' + str(rate) + '%)')
         [print(x) for x in info]
+
         time.sleep(sleep)
 except KeyboardInterrupt:
     print("Exit...")
